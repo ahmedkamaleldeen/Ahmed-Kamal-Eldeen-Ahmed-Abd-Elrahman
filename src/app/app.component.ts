2e80e81ad566c9512dacfbdf9ec8a511645cebba
ahmedkamaleldeen <ahmedkamaleldeen96@gmail.com>
2022-02-18 16:25:46 +0200

    Lap1DAY#1:Portfolio

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular';
+  displayinfo():string{
+    return `I am ${this.title}`
+  }
+}
